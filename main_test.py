40bb1eaa5e97f94559e713a6c61ae16d94fd24e5
marcosantonastasi <marco.santonastasi@gmail.com>
2022-03-12 12:07:11 +0100

    refactored tests

--- main_test.py
+++ main_test.py
@@ -1,8 +1,10 @@
 import pytest
 from main import isLeap
 
-def test_not_divisible_by_4():
+def test_divisible_by_4():
     assert not isLeap(1997) , "should be divisible by 4"
+    assert isLeap(1996) , "should be leap"
 
 def test_divisible_by_100_and_by_400():
     assert not isLeap(1800) , "should be divisible by 100 and by 400"
+    assert isLeap(1600) , "should be leap"
